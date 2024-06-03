21409465d966729740f55ef3a600f685bd7f9be8
Reco1l <geronimoferruccio15@gmail.com>
2024-06-03 14:07:35 -0300

    Document 'Osu' class

--- src/ru/nsu/ccfit/zuev/osu/Osu.java
+++ src/ru/nsu/ccfit/zuev/osu/Osu.java
@@ -13,6 +13,8 @@ import ru.nsu.ccfit.zuev.osu.scoring.ScoreLibrary;
 import ru.nsu.ccfit.zuev.osu.scoring.ScoringScene;
 
 /**
+ * Global entry for every main component used in the game.
+ *
  * Originally created by Fuuko on 2015/4/24.
  * Modified by Reco1l on 2024/6/3.
  */
@@ -45,6 +47,14 @@ public class Osu {
     private static int loadingProgress;
 
 
+    private Osu() {
+        // Prevent instantiation.
+    }
+
+
+    /**
+     * Initializes most of the game components, should be called once.
+     */
     public static void init() {
 
         SaveServiceObject = (SaveServiceObject) Activity.getApplication();
@@ -75,29 +85,47 @@ public class Osu {
     }
 
 
+    /**
+     * Provides the current selected track defined by the song menu. Can be `null` when there's no song selected yet.
+     */
     @Nullable
     public static TrackInfo getSelectedTrack() {
         return selectedTrack;
     }
 
+    /**
+     * Sets the current selected track.
+     */
     public static void setSelectedTrack(@Nullable TrackInfo track) {
         selectedTrack = track;
     }
 
+    /**
+     * Returns the current loading progress.
+     */
     public static int getLoadingProgress() {
         return loadingProgress;
     }
 
+    /**
+     * Sets the current loading progress.
+     */
     public static void setLoadingProgress(int value) {
         loadingProgress = value;
     }
 
+    /**
+     * Returns the current loading information.
+     */
     public static String getLoadingInfo() {
         return loadingInfo;
     }
 
-    public static void setInfo(String info) {
-        loadingInfo = info;
+    /**
+     * Sets the current loading information.
+     */
+    public static void setInfo(String value) {
+        loadingInfo = value;
     }
 
 }
