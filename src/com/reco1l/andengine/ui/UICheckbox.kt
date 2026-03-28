4c03458ed69aa847b2f67e39c057e81b5dcbfcf5
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-03-29 01:17:31 +0700

    Lots of updates to implementation

--- src/com/reco1l/andengine/ui/UICheckbox.kt
+++ src/com/reco1l/andengine/ui/UICheckbox.kt
@@ -55,7 +55,6 @@ class UICheckbox(initialValue: Boolean = false) : UIControl<Boolean>(initialValu
         }
 
         checkIcon.fadeIn(0.2f)
-        onChange?.invoke(value)
     }
 
     override fun onAreaTouched(event: TouchEvent, localX: Float, localY: Float): Boolean {
