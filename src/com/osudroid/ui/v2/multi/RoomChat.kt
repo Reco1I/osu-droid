28d7516979e5a99b0a8bca18518146963a516566
AskingSamagra <samagraarohan@gmail.com>
2026-04-23 16:30:52 +0530

    Move `collapse` call to `onDetached`

--- src/com/osudroid/ui/v2/multi/RoomChat.kt
+++ src/com/osudroid/ui/v2/multi/RoomChat.kt
@@ -186,7 +186,6 @@ class RoomChat : UILinearContainer() {
     }
 
     fun hide() {
-        collapse(true)
         detachSelf()
     }
 
@@ -237,6 +236,9 @@ class RoomChat : UILinearContainer() {
         )
     }
 
+    override fun onDetached() {
+        collapse(true)
+    }
 
     override fun onManagedUpdate(deltaTimeSec: Float) {
 
