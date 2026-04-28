8bb21e32e0eb3c4e500b0f79e7ce7746dc5b9188
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-04-29 00:19:32 +0800

    Register misses to `GameObjectListener.registerAccuracy`

--- src/ru/nsu/ccfit/zuev/osu/game/GameplayHitCircle.java
+++ src/ru/nsu/ccfit/zuev/osu/game/GameplayHitCircle.java
@@ -289,6 +289,7 @@ public class GameplayHitCircle extends GameObject {
                 final byte forcedScore = (replayObjectData == null) ? 0 : replayObjectData.result;
 
                 removeFromScene();
+                listener.registerAccuracy(HitObjectType.Normal, mehWindow + 1);
                 listener.onCircleHit(id, 10, position, false, forcedScore, comboColor);
             }
         }
