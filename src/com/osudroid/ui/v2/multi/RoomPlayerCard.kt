6c4777e627731644f62ccd4190e1b8bce1692a06
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-02 17:12:34 +0800

    Increase icon sizes slightly

--- src/com/osudroid/ui/v2/multi/RoomPlayerCard.kt
+++ src/com/osudroid/ui/v2/multi/RoomPlayerCard.kt
@@ -188,7 +188,7 @@ class RoomPlayerCard : UILinearContainer() {
                         textureRegion = ResourceManager.getInstance().getTexture("missing")
                         anchor = Anchor.CenterLeft
                         origin = Anchor.CenterLeft
-                        size = Vec2(18f)
+                        size = Vec2(24f)
                     }
                 }
             }
@@ -242,7 +242,7 @@ class RoomPlayerCard : UILinearContainer() {
                     modDisplay?.detachSelf()
 
                     modDisplay = UIText().apply {
-                        minHeight = 18f // Force to take space even if no mods are enabled
+                        minHeight = 24f // Force to take space even if no mods are enabled
                         style += {
                             fontSize = FontSize.XS
                             color = it.accentColor * 0.8f
@@ -258,8 +258,8 @@ class RoomPlayerCard : UILinearContainer() {
                     modDisplay?.detachSelf()
 
                     modDisplay = ModsIndicator().apply {
-                        minHeight = 18f // Force to take space even if no mods are enabled
-                        iconSize = 18f
+                        minHeight = 24f // Force to take space even if no mods are enabled
+                        iconSize = 24f
                     }
 
                     innerContainer += modDisplay!!
