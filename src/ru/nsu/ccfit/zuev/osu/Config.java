d1760e9ef866f9ec1c7d51725bb98f213225c865
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-08 09:46:26 +0800

    Remove `minimumGameplaySynchronizationLimit` configuration

--- src/ru/nsu/ccfit/zuev/osu/Config.java
+++ src/ru/nsu/ccfit/zuev/osu/Config.java
@@ -89,7 +89,6 @@ public class Config {
         RES_HEIGHT,
         spinnerStyle,
         metronomeSwitch,
-        minimumGameplaySynchronizationTime,
         backButtonPressTime;
 
     private static float soundVolume,
@@ -232,7 +231,6 @@ public class Config {
         receiveAnnouncements = prefs.getBoolean("receiveAnnouncements", true);
         safeBeatmapBg = prefs.getBoolean("safebeatmapbg", false);
         shiftPitchInRateChange = prefs.getBoolean("shiftPitchInRateChange", false);
-        minimumGameplaySynchronizationTime = prefs.getInt("gameAudioSynchronizationThreshold", 20);
         backButtonPressTime = Config.getInt("back_button_press_time", 300);
 
         // Multiplayer
@@ -790,10 +788,6 @@ public class Config {
 
     public static boolean isDisplayPlayfieldBorder() {
         return getBoolean("displayPlayfieldBorder", false);
-    }
-
-    public static int getMinimumGameplaySynchronizationTime() {
-        return minimumGameplaySynchronizationTime;
     }
 
     public static int getBackButtonPressTime() {
