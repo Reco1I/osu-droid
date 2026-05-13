c80a197f4d9706b93bbb1de603ca7c8ee6b0b71b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-05-13 10:27:55 +0800

    Remove draw thread clock

--- src/com/reco1l/andengine/UIEngine.kt
+++ src/com/reco1l/andengine/UIEngine.kt
@@ -23,17 +23,7 @@ import org.anddev.andengine.engine.camera.Camera
 
 class UIEngine(val context: Activity, options: EngineOptions) : Engine(options),
     IClockProvider<ThrottledFrameClock> {
-    /**
-     * The clock used for the update thread.
-     */
-    val updateClock = ThrottledFrameClock()
-
-    /**
-     * The clock used for the draw (GL) thread.
-     */
-    val drawClock = ThrottledFrameClock()
-
-    override val clock = updateClock
+    override val clock = ThrottledFrameClock()
 
     private val displayDensity = context.resources.displayMetrics.density
 
@@ -119,8 +109,6 @@ class UIEngine(val context: Activity, options: EngineOptions) : Engine(options),
 
 
     override fun onDrawScene(pGL: GL10) {
-        drawClock.processFrame()
-
         val focusedEntity = focusedEntity
 
         if (focusedEntity != null) {
@@ -247,9 +235,9 @@ class UIEngine(val context: Activity, options: EngineOptions) : Engine(options),
     }
 
     override fun onUpdate(pNanosecondsElapsed: Long) {
-        updateClock.processFrame()
+        clock.processFrame()
 
-        super.onUpdate((updateClock.elapsedFrameTime * 1e9).toLong())
+        super.onUpdate((clock.elapsedFrameTime * 1e9).toLong())
     }
 
     override fun setScene(scene: Scene?) {
